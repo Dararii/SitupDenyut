f625af693611e9dda0ba076edf6bd6dc5ba86d98
Darari Nur Amali <darari7@gmail.com>
2020-01-18 16:23:52 +0700

    Add docs and add BPM feature.

--- app/src/main/java/com/sift/situpdenyut/MainActivity.kt
+++ app/src/main/java/com/sift/situpdenyut/MainActivity.kt
@@ -28,8 +28,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var denyutRef: DatabaseReference
     private lateinit var situpListener: ValueEventListener
     private lateinit var situpRef: DatabaseReference
+    private lateinit var bpmListener: ValueEventListener
+    private lateinit var bpmRef: DatabaseReference
     private var currentSessionDenyutData: ArrayList<Denyut> = ArrayList()
     private var currentSessionSitupData: Long = 0
+    private var currentSessionBpmData: Long = 0
 
     /**
      * Activity entry point
@@ -42,71 +45,130 @@ class MainActivity : AppCompatActivity() {
         dbEngine = (application as DenyutApp).appComponent.getDbEngine()
         Log.d("MAIN", "engine object $dbEngine")
 
+        //initiate listener setup
         initRealtimeListener()
 
-        btnCountSitup.setOnClickListener {
+        /*btnCountSitup.setOnClickListener {
             dbEngine.getDb().reference.child("data_denyut").child("${System.currentTimeMillis()}")
                 .setValue((0..50).random())
-        }
+        }*/
 
         switch1.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                // Switch to ON
                 dbEngine.getDb().reference.child("switch").setValue(1)
+                //Enable realtime listener
+                addListener()
             } else {
+                //Switch to off
                 dbEngine.getDb().reference.child("switch").setValue(0)
+                //Delete data denyut in database
                 dbEngine.getDb().reference.child("data_denyut").removeValue()
+                //remove realtime listener
+                removeListener()
             }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
+    /**
+     * Attach Listener function
+     */
+    private fun addListener() {
         denyutRef.limitToLast(15).addValueEventListener(denyutListener)
         situpRef.addValueEventListener(situpListener)
+        bpmRef.addValueEventListener(bpmListener)
+    }
+
+    /**
+     * Detach listener funciton
+     */
+    private fun removeListener() {
+        denyutRef.limitToLast(15).removeEventListener(denyutListener)
+        situpRef.removeEventListener(situpListener)
+        bpmRef.removeEventListener(bpmListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        addListener()
     }
 
     override fun onPause() {
-        denyutRef.limitToLast(15).removeEventListener(denyutListener)
-        situpRef.removeEventListener(situpListener)
+        removeListener()
         super.onPause()
     }
 
     /**
-     * Real-time event listener from database
+     * Setup Real-time event listener from database
      */
     private fun initRealtimeListener() {
+        // define database references for denyut
         denyutRef = dbEngine.getDb().reference.child("data_denyut")
+
+        // Setup value listener
         denyutListener = object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {}
             override fun onDataChange(data: DataSnapshot) {
                 currentSessionDenyutData.clear()
                 Log.d(TAG, "Data test1: ${data.childrenCount}")
+
+                // parse value from server
                 for (i in data.children) {
                     try {
                         val v = i.value
                         val t = "${i.key}"
 
+                        // parse to model
                         val denyut = Denyut(
                             value = v.toString().toInt(),
                             timestamp = t
                         )
+
+                        //add data
                         currentSessionDenyutData.add(denyut)
                     } catch (e: Exception) {
                         Log.e(TAG, "Error while parsing data denyut ${e.message}")
                     }
                 }
+                // drwa data to chart
                 drawData(currentSessionDenyutData, chartMainDenyut)
             }
         }
 
+        //define db reference for situp
         situpRef = dbEngine.getDb().reference.child("situp")
+
+        //setup listener for situp
         situpListener = object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {}
             override fun onDataChange(data: DataSnapshot) {
                 try {
+                    //parse data to long
                     currentSessionSitupData = data.value.toString().toLong()
                     Log.d(TAG, "Data situp: ${data.value}")
+
+                    //update UI
                     textMainSitupCount.text = "$currentSessionSitupData kali"
+                } catch (e: Exception) {
+                    Log.e(TAG, "Error while parsing data situp ${e.message}")
+                }
+            }
+        }
+
+        //define db reference for situp
+        bpmRef = dbEngine.getDb().reference.child("bpm")
+
+        //setup listener for situp
+        bpmListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {}
+            override fun onDataChange(data: DataSnapshot) {
+                try {
+                    //parse data to long
+                    currentSessionBpmData = data.value.toString().toLong()
+                    Log.d(TAG, "Data situp: ${data.value}")
+
+                    //update ui
+                    textMainBPM.text = "$currentSessionBpmData"
                 } catch (e: Exception) {
                     Log.e(TAG, "Error while parsing data situp ${e.message}")
                 }
@@ -118,6 +180,7 @@ class MainActivity : AppCompatActivity() {
      * Draw data to chart
      */
     fun drawData(data: ArrayList<Denyut>, chart: LineChart) {
+        // Iterate data
         val entries: MutableList<Entry> = ArrayList()
         for (i in 0 until data.size) {
             val v = try { // get the object by the key.
@@ -128,13 +191,16 @@ class MainActivity : AppCompatActivity() {
             entries.add(Entry(i.toFloat(), v))
         }
 
+        // Create dataset
         val dataSet = LineDataSet(entries, "Denyut") // add entries to dataset
         dataSet.setColors(Color.BLUE)
         dataSet.setDrawValues(false)
 
+        // Draw data
         val dataSets: MutableList<ILineDataSet> = ArrayList()
         dataSets.add(dataSet)
 
+        // prepare and setup chart
         val lineData = LineData(dataSets)
         chart.data = lineData
         val yAxisLeft = chart.axisLeft
@@ -143,8 +209,6 @@ class MainActivity : AppCompatActivity() {
         yAxisLeft.setDrawGridLines(false)
         yAxisRight.setDrawLabels(false)
         yAxisRight.setDrawGridLines(false)
-
-
         val xAxis = chart.xAxis
         xAxis.position = XAxis.XAxisPosition.BOTTOM
         xAxis.textSize = 12f
@@ -155,7 +219,9 @@ class MainActivity : AppCompatActivity() {
         xAxis.granularity = 1f
         xAxis.axisMinimum = 0f
         xAxis.axisMaximum = if (data.size > 30) 60f else data.size.toFloat()
-        chart.invalidate() // refresh
+
+        //refresh chart
+        chart.invalidate()
 
     }
 }
--- app/src/main/java/com/sift/situpdenyut/engine/DbEngine.kt
+++ app/src/main/java/com/sift/situpdenyut/engine/DbEngine.kt
@@ -4,15 +4,22 @@ import com.google.firebase.database.FirebaseDatabase
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Database instance, singleton using Dagger 2
+ */
 @Singleton
 class DbEngine @Inject constructor() {
 
     private var db: FirebaseDatabase = FirebaseDatabase.getInstance()
 
     init {
+        //Set data persistence enable.
         db.setPersistenceEnabled(true)
     }
 
+    /**
+     * Getter function for firebase instance
+     */
     fun getDb(): FirebaseDatabase {
         db.goOnline()
         return db
